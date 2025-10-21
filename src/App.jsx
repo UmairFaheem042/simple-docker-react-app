82f397b4f35c54eab7c0354a005235f3ecd4456e
Umair Faheem <umair.freelancer042@gmail.com>
2025-10-21 18:32:17 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+      <h4>Testing Docker App</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus explicabo nemo similique eos id laudantium nisi nesciunt nam, eaque at quisquam! Praesentium laudantium culpa est consequuntur, iusto at inventore delectus.</p>
+    </main>
+  )
+}
+
+export default App
